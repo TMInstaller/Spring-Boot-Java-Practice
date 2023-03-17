397cef3e5bca2f3a667256bd3addd602f50bbdde
TMInstaller <ajvldmlqjqclr@naver.com>
2023-03-17 11:23:32 +0900

    Create Test classes

--- Project0317/src/test/java/com/tukorea/Project0317/repository/MemoryMemberRepositoryTest.java
+++ Project0317/src/test/java/com/tukorea/Project0317/repository/MemoryMemberRepositoryTest.java
@@ -1,2 +1,73 @@
-package com.tukorea.Project0317.repository;public class MemoryMemberRepositoryTest {
+package com.tukorea.Project0317.repository;
+
+import com.tukorea.Project0317.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repo = new MemoryMemberRepository();
+
+    @Test
+    public void testInsertMember() {
+        // 회원 정보 설정
+        Member member = new Member();
+        member.setName("Spring");
+
+        // 회원 정보 추가
+        Member result = repo.insertMember(member);
+        System.out.println("(1) result : " + result.getId() + " / " + result.getName());
+    }
+
+    @Test
+    public void testSelectMemberById() {
+        // 회원 정보 설정
+        Member member = new Member();
+        member.setName("Spring");
+
+        // 회원 정보 추가
+        repo.insertMember(member);
+
+        // 아이디로 회원 정보 조회
+        Member result = repo.selectMemberById(member.getId());
+//        Member result = repo.selectMemberById(99); // 없을 법한 숫자 넣어보기 - 에러 발생
+        System.out.println("(2) result : " + result.getId() + " / " + result.getName());
+    }
+
+    @Test
+    public void testSelectMemberByName() {
+        // 회원 정보 설정
+        Member member1 = new Member();
+        member1.setName("hello1");
+
+        Member member2 = new Member();
+        member2.setName("hello2");
+
+        // 회원 정보 추가
+        repo.insertMember(member1);
+        repo.insertMember(member2);
+
+        // 이름으로 회원 정보 조회
+        Member result = repo.selectMemberByName("hello1");
+//        Member result = repo.selectMemberByName("Ji-Won"); // 없는 이름으로 조회해보기 - 에러
+        System.out.println("(3) result : " + result.getId() + " / " + result.getName());
+    }
+
+    @Test
+    public void testSelectMemberList() {
+        // 회원 정보 설정
+        Member member1 = new Member();
+        member1.setName("check-all 1");
+
+        Member member2 = new Member();
+        member2.setName("check-all 2");
+
+        // 회원 정보 추가
+        repo.insertMember(member1);
+        repo.insertMember(member2);
+
+        // 회원 목록 조회
+        List<Member> result = repo.selectMemberList();
+        System.out.println("(4) result : " + result.size());
+    }
 }
