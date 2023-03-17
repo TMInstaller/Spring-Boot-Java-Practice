7e5eae83f00c81d5ec49cec55dcbde24d0dd3f1b
TMInstaller <ajvldmlqjqclr@naver.com>
2023-03-17 11:09:15 +0900

    Make variables, getter, setter

--- Project0317/src/main/java/com/tukorea/Project0317/domain/Member.java
+++ Project0317/src/main/java/com/tukorea/Project0317/domain/Member.java
@@ -1,2 +1,22 @@
-package com.tukorea.Project0317.domain;public class Member {
+package com.tukorea.Project0317.domain;
+
+public class Member {
+    private int id; // 식별자용 아이디
+    private String name; // 회원 이름
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
