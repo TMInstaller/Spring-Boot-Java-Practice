1e68f365eea24e96cff73aa1a4b79cfe05dd0b6f
TMInstaller <ajvldmlqjqclr@naver.com>
2023-03-17 11:09:32 +0900

    Create interface

--- Project0317/src/main/java/com/tukorea/Project0317/repository/MemberRepositoryIF.java
+++ Project0317/src/main/java/com/tukorea/Project0317/repository/MemberRepositoryIF.java
@@ -1,2 +1,19 @@
-package com.tukorea.Project0317.repository;public class MemberRepositoryIf {
+package com.tukorea.Project0317.repository;
+
+import com.tukorea.Project0317.domain.Member;
+
+import java.util.List;
+
+public interface MemberRepositoryIF {
+    // 회원 등록
+    Member insertMember(Member member);
+
+    // 회원 아이디로 조회
+    Member selectMemberById(int id);
+
+    // 회원 이름으로 조회
+    Member selectMemberByName(String name);
+
+    // 회원 전체 목록 조회
+    List<Member> selectMemberList();
 }
